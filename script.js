5c66713fb2d0ab1669bac7fcfc140ef306a6150f
Mike Gozzo <mike@smooch.io>
2017-03-18 16:08:33 -0400

    script updates, sleeps

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('Hi! I\'m Smooch\'s Channel Bot 😎')
-                .then(sleep(500))
+                .then(sleep(50))
                 .then(() => bot.say('I\'ll show you how Smooch makes it possible to move conversations across messaging channels so that you can save 💸 on your SMS bill while delivering better, more engaging experiences to your users using the latest messaging apps 🔥'))
                 .then(() => 'askName');
         }
@@ -24,7 +24,7 @@ module.exports = new Script({
             const name = message.text;
             return bot.setProp('name', name)
                 .then(() => bot.say(`Great! I'll call you ${name} - In order to prove that I remember who you are once I invite you to join me on another channel, tell me a secret word.`))
-                .then(sleep(500))
+                .then(sleep(50))
                 .then(() => bot.say(`I'll repeat it to you on the new messaging channel.`))
                 .then(() => 'promptMerge');
         }
@@ -44,12 +44,12 @@ module.exports = new Script({
       receive: (bot, message) => {
         return bot.getProp('name')
             .then((name) => bot.say(`Well ${name}, as you can see I haven't forgotten about you and your secret... 😈`))
-            .then(sleep(250))
+            .then(sleep(25))
             .then(() => bot.getProp('secret'))
             .then((secret) => bot.say(`You told me that your secret is: "${secret}"`))
-            .then(sleep(250))
+            .then(sleep(25))
             .then(() => bot.say('![](http://i.giphy.com/l0HlujEDdIqEoicGQ.gif)'))
-            .then(sleep(250))
+            .then(sleep(25))
             .then(() => bot.say("I don't have anything more to say right now, but I hope you liked playing with me!"))
             .then(() => "finish");
         }
