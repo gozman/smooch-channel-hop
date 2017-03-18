0d051ad2f80412cec18173acb65e456fb48424d5
Mike Gozzo <mike@smooch.io>
2017-03-18 15:49:20 -0400

    script updates, sleeps

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Script = require('smooch-bot').Script;
+var sleep = require('sleep-promise');
 
 module.exports = new Script({
     processing: {
@@ -11,6 +12,7 @@ module.exports = new Script({
     start: {
         receive: (bot) => {
             return bot.say('Hi! I\'m Smooch\'s Channel Bot 😎')
+                .then(sleep(500))
                 .then(() => bot.say('I\'ll show you how Smooch makes it possible to move conversations across messaging channels so that you can save 💸 on your SMS bill while delivering better, more engaging experiences to your users using the latest messaging apps 🔥'))
                 .then(() => 'askName');
         }
@@ -21,7 +23,9 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name} - In order to prove that I remember who you are once I invite you to join me on another channel, tell me a secret... I promise I won't tell anyone else!`))
+                .then(() => bot.say(`Great! I'll call you ${name} - In order to prove that I remember who you are once I invite you to join me on another channel, tell me a secret word.`))
+                .then(sleep(500))
+                .then(() => bot.say(`I'll repeat it to you on the new messaging channel.`))
                 .then(() => 'promptMerge');
         }
     },
@@ -30,7 +34,7 @@ module.exports = new Script({
       receive: (bot, message) => {
         const secret = message.text;
         return bot.setProp('secret', secret)
-            .then(() => bot.linkToMessenger("Your secret is safe with me (for now...) 👀 Let's continue this conversation on Messenger:", process.env.FB_PAGE_ID))
+            .then(() => bot.linkToMessenger("Thanks! 👀 Let's continue this conversation on Messenger:", process.env.FB_PAGE_ID))
             .then(() => 'waitMerge');
         }
     },
@@ -40,10 +44,12 @@ module.exports = new Script({
       receive: (bot, message) => {
         return bot.getProp('name')
             .then((name) => bot.say(`Well ${name}, as you can see I haven't forgotten about you and your secret... 😈`))
-            .then(() => 'tellSecret')
+            .then(sleep(250))
             .then(() => bot.getProp('secret'))
             .then((secret) => bot.say(`You told me that your secret is: "${secret}"`))
+            .then(sleep(250))
             .then(() => bot.say('![](http://i.giphy.com/l0HlujEDdIqEoicGQ.gif)'))
+            .then(sleep(250))
             .then(() => bot.say("I don't have anything more to say right now, but I hope you liked playing with me!"))
             .then(() => "finish");
         }
